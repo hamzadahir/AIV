7c035019794a50af80367554782bb6a8ededa76c
Mykola <melnyk.cto.numo@gmail.com>
2020-06-09 06:18:02 +0300

    fix: change server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-if (process.env.NODE_ENV === 'production') {
-    // Exprees will serve up production assets
-    app.use(express.static('build'));
+const express = require("express");
 
-    // Express serve up index.html file if it doesn't recognize route
-    const path = require('path');
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-    });
-}
+// Create the app
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.listen(PORT, function () {
+    console.log('Express server up on port ' + PORT)
+});
